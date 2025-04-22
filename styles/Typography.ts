87197ed45418d8d4716d75499aa49b368aa14a29
seominhee <minheee07@gmail.com>
2025-04-22 11:25:46 +0900

    프로젝트 최초 생성

---
+++ styles/Typography.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const Typography = StyleSheet.create({
+  h1: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  h2: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  body: {
+    fontSize: 16,
+  },
+});
---
+++ timer/Pomodoro.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Typography } from '../styles/Typography';
+
+const Pomodoro = () => {
+  return (
+    <View>
+      <Text style={Typography.h1}>제목</Text>
+    </View>
+  );
+};
+
+export default Pomodoro;
